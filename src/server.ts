745f6945f77f82994a305f24796e438ce2f62127
Aline Naoe <alinenaoe@gmail.com>
2021-02-24 20:00:16 -0300

    config typeorm and create users table

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: "hello get" })
-});
+app.get('/', (request, response) => response.json({ message: 'hello get' }));
 
-app.post('/', (request, response) => {
-    return response.json({ message: "hello post" })
-});
+app.post('/', (request, response) => response.json({ message: 'hello post' }));
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+app.listen(3333, () => console.log('Server is running on port 3333'));
